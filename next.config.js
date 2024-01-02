9c08ca47dc247edae309c0617048c9ed31a70e59
Julia Fitzgerald <104915352+jgfitzgerald@users.noreply.github.com>
2024-01-01 20:12:42 -0400

    Update image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< Updated upstream
 const nextConfig = {}
 
+<<<<<<< HEAD
 module.exports = nextConfig
+=======
+const nextConfig = {
+    output: 'export',
+    }
+    
+    module.exports = nextConfig
+>>>>>>> Stashed changes
+=======
+module.exports = nextConfig
+>>>>>>> 516a6ba1f010ba01683ecbee0006e2523e1375f6
