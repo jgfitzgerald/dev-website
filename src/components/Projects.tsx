888362076a31021db92e7837157ecbdf209f1e40
Julia Fitzgerald <104915352+jgfitzgerald@users.noreply.github.com>
2024-01-05 14:37:07 -0400

    update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -59,33 +59,6 @@ export default function Home() {
         ))}
       </div>
     </div>
-  );
-  
-  
+  );  
 }
-
-
-// return (
-//     <div id="experience" className="mb-4 w-full my-3">
-//       <div className="uppercase tracking-wide text-lg text-rose-600 dark:text-green-100 font-semibold">
-//         Experience
-//       </div>
-//             {jobs && jobs.map((job) => (
-//     <div key={job.title} className=" rounded-xl flex flex-wrap md:flex-no-wrap hover:bg-white hover:bg-opacity-10 border-2 border-transparent hover:border-2 hover:border-white transition duration-300 px-5 py-3 ease-in-out rounded mb-4 hover:border-opacity-10">
-//       <div className="w-full md:w-2/5 my-2 text-black text-opacity-50 font-semibold uppercase text-sm">{job.range}</div>
-//       <div className="w-full md:w-3/5">
-//         <p className="my-2 mr-2">
-//           {job.title} - {job.company}
-//         </p>
-//         <p className="my-2 text-sm text-black text-opacity-50 dark:text-slate-300">{job.description}</p>
-//         <div className="flex flex-wrap">
-//         {job.tags?.map((tag, index) => (
-//           <span key={index} className="text-sm bg-opacity-45 dark:bg-opacity-45 bg-rose-500 dark:bg-emerald-500 px-2 py-1 text-white rounded-lg m-1">{tag}</span>
-//         ))}
-//         </div>
-//       </div>
-//     </div>
-//   ))}
-//     </div>
-//   );
 
