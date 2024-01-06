18411b5920beed5ac17200af2303663b9840801a
Julia Fitzgerald <104915352+jgfitzgerald@users.noreply.github.com>
2024-01-06 16:48:40 -0400

    update bio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return (<div id="about" className="mb-4"><div className="uppercase tracking-wide text-lg text-rose-600 dark:text-green-100 font-semibold">About</div>
-    <p className="mt-2 text-black dark:text-slate-300">My goal is to blend creativity and technology to deliver exceptional user experiences. As I embark on the next chapter of my career, I am eagerly seeking new opportunities. I&apos;m a graduate with a thirst for challenges, currently based in Saint John, New Brunsick. I&apos;m ready to bring my skills, enthusiasm, and dedication to a team that values innovation and collaboration.</p>
+    <p className="mt-2 text-black dark:text-slate-300">My goal is to blend creativity and technology to deliver exceptional user experiences. As I embark on the next chapter of my career, I am eagerly seeking new opportunities. I&rsquo;m a graduate with a thirst for challenges, ready to bring my skills, enthusiasm, and dedication to a team that values innovation and collaboration.</p>
     </div>)
 }
